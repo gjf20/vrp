b2d214f71c8e66430f298625f4d57dd55d2fa9b0
Jamie Flynn <gregoryjamesflynn@gmail.com>
2024-02-10 20:34:56 -0700

    chore(project): initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports =  {
+    semi:  true,
+    trailingComma:  'all',
+    singleQuote:  true,
+    printWidth:  120,
+    tabWidth:  4,
+    importOrder: ["^src/.*$", "^[./]"],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    bracketSpacing: false,
+    endOfLine:"auto"
+  };
+  
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # vrp
 Vehicle Routing Problem
+
+# Usage
+Using vanilla npm
+
+```
+npm i
+npm run compile
+npm run solution {path_to_problem}
+```
