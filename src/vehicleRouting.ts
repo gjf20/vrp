7dfd6a09c6597bfe8321c2760c2290a1e203798a
Jamie Flynn <gregoryjamesflynn@gmail.com>
2024-02-12 18:17:28 -0700

    fix(optimization): consolidate statements

--- src/vehicleRouting.ts
+++ src/vehicleRouting.ts
@@ -28,9 +28,7 @@ export function assignDrivers(trips: Trips) {
     }
 
     // 2. sort the potentials by their savings value (and filter negative combinations)
-    potenialSavings
-        .filter((sav) => sav !== undefined && sav.savings > 0)
-        .sort((a, b) => (a.savings > b.savings ? -1 : 1));
+    potenialSavings.filter((sav) => sav.savings > 0).sort((a, b) => (a.savings > b.savings ? -1 : 1));
 
     // 3. start at the most savings and try to apply the change
     for (let i = 0; i < potenialSavings.length; i++) {
@@ -49,15 +47,13 @@ export function assignDrivers(trips: Trips) {
 }
 
 function mergeTrips(entry: SavingsEntry) {
-    const parent = entry.parent === 'a' ? entry.a : entry.b;
-
-    parent.trip.route = mergeRoute(entry);
-
     if (entry.parent === 'a') {
+        entry.a.trip.route = mergeRoute(entry);
         entry.a.trip.ids = entry.a.trip.ids.concat(entry.b.trip.ids);
         // change the reference to the trip in the pointer shim.  Entries all point to the same trip shim, so change it once, it changes for the rest
         entry.b.trip.ids = [];
     } else {
+        entry.b.trip.route = mergeRoute(entry);
         entry.b.trip.ids = entry.b.trip.ids.concat(entry.a.trip.ids);
         entry.a.trip.ids = [];
     }
@@ -67,8 +63,7 @@ function mergeRoute(entry: SavingsEntry): Route {
     const [parent, child] = entry.parent === 'a' ? [entry.a.trip, entry.b.trip] : [entry.b.trip, entry.a.trip];
 
     // making copies so we don't mutate state until we're sure
-    const newRoute = [...parent.route, ...child.route];
-    return newRoute;
+    return [...parent.route, ...child.route];
 }
 
 /**
@@ -92,16 +87,12 @@ function isMergedRouteValid(entry: SavingsEntry): boolean {
         return false;
     }
 
-    const resultRoute = [origin, ...mergeRoute(entry), origin];
-    const totalDrive = measureRoute(resultRoute);
-    const totalDriveExceeded = totalDrive > maxDriveDistance;
-
-    return !totalDriveExceeded;
+    const totalDrive = measureRoute(mergeRoute(entry));
+    return totalDrive <= maxDriveDistance;
 }
 
 export function distance(a: Point, b: Point): number {
-    const dist = Math.pow(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2), 0.5);
-    return dist;
+    return Math.pow(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2), 0.5);
 }
 
 function savingsIfMerged(a: Route, b: Route): SavingsMerge {
@@ -112,8 +103,7 @@ function savingsIfMerged(a: Route, b: Route): SavingsMerge {
     const bEndaStart = distance(endB, a[0]);
 
     if (aEndbStart < bEndaStart) {
-        const dist = distance(endA, origin) + distance(origin, b[0]) - aEndbStart; // savings formula from Clark and Wright [1]
-        return {parent: 'a', savings: dist};
+        return {parent: 'a', savings: distance(endA, origin) + distance(origin, b[0]) - aEndbStart}; // savings formula from Clark and Wright [1]
     } else {
         return {parent: 'b', savings: distance(endB, origin) + distance(origin, a[0]) - bEndaStart}; // same as above
     }
